06aac6347d26ebc6e6e1b1d5af5487e7c10802bc
Brandon Piña <brandon.pina@invista.com>
2024-10-26 17:36:31 -0500

    update unit tests

--- src/frame/parse.rs
+++ src/frame/parse.rs
@@ -2,12 +2,12 @@ use ascii::AsciiChar;
 use ascii::ToAsciiChar;
 use log::error;
 use nom::bytes;
+use nom::combinator;
 use nom::error::ErrorKind;
 use nom::number;
 use nom::sequence;
 use nom::sequence::tuple;
 use nom::IResult;
-use nom::combinator;
 
 use crate::frame::FrameError;
 
@@ -18,7 +18,6 @@ use super::FrameHeader;
 pub fn ascii_char(char: AsciiChar) -> impl Fn(&[u8]) -> IResult<&[u8], AsciiChar> {
     move |input: &[u8]| {
         let (remaining, parsed) = bytes::streaming::tag([char as u8])(input)?;
-        //let out:AsciiChar = u8::to_ascii_char(parsed[0])?;
         match u8::to_ascii_char(parsed[0]) {
             Ok(out) => Ok((remaining, out)),
             Err(err) => {
@@ -66,13 +65,34 @@ pub fn frame_body(header: FrameHeader) -> impl Fn(&[u8]) -> IResult<&[u8], Frame
     }
 }
 
-pub fn frame(input: &[u8]) -> IResult<&[u8],Frame>{
+pub fn frame(input: &[u8]) -> IResult<&[u8], Frame> {
     let body_parser = combinator::flat_map(frame_header, frame_body);
-    let (remaining,(header,body)) = tuple((frame_header,body_parser))(input)?;
-    Ok((remaining,Frame::new_unchecked(header, body)))
+    let (remaining, (header, body, _)) =
+        tuple((frame_header, body_parser, ascii_char(AsciiChar::ETX)))(input)?;
+    Ok((remaining, Frame::new_unchecked(header, body)))
 }
 
 #[cfg(test)]
-mod test{
+mod test {
+    use rand::{rngs::ThreadRng, thread_rng, Rng};
 
+    use crate::frame::Frame;
+    const BUF_SIZE: usize = 4;
+    fn rand_buf() -> [u8; BUF_SIZE] {
+        let mut rng = rand::thread_rng();
+        let mut data: [u8; BUF_SIZE] = [0; BUF_SIZE];
+        //rng.fill(&mut data);
+        data
+    }
+
+    #[test]
+    fn frame_recovery() {
+        let buf = rand_buf();
+        let user_id: u64 = thread_rng().gen();
+        let frame = Frame::new(&buf, user_id).expect("Could not create frame");
+        let raw: Vec<u8> = frame.clone().try_into().expect("Could not serialize frame");
+        let (remaining, recovered) = super::frame(&raw).expect("Could not parse frame");
+        assert_eq!(remaining.len(), 0);
+        assert_eq!(frame, recovered);
+    }
 }
